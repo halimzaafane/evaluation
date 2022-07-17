3ea55d9a645719551ea85fc906ca55eb2cc34655
halimzaafane <109077943+halimzaafane@users.noreply.github.com>
2022-07-17 08:44:38 +0200

    update17/07/22

--- exo_form/exo_form.js
+++ exo_form/exo_form.js
@@ -1,8 +1,10 @@
 
-function conca(){
+/*function conca(){
     const nom = document.getElementsByName("nom").value;
     const prenom = document.getElementsByName("prenom").value;
     var conc = nom + prenom;
     document.getElementById("result") = conc;
     console.log("result");
-}
+}*/
+ formu[2].onclick = function conca()
+{formu[3].getElementById("result").value = formu[0].value +" "+ formu[1].value}
